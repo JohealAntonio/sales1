3188c62bf4eaefc5a4ea9724a8b8860e24c8a687
JohealAntonio <j4usjohealantonio@gmail.com>
2023-12-20 12:57:49 +0530

    12/20/2023

--- product_sales/forms.py
+++ product_sales/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model=User
